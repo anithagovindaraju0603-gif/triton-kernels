18cc05909e39468911304b6e215f19e1cc5db746
anithagovindaraju0603-gif <anithagovindaraju0603@gmail.com>
2026-06-19 16:32:02 -0400

    requrements

--- softmax/test_correctness.py
+++ softmax/test_correctness.py
@@ -2,8 +2,11 @@ import torch
 from kernel import softmax
 
 x = torch.randn(100,100, device = "cuda")
+print(f"Input matrix for softmax: {x}")
 out_triton = softmax(x)
 out_pytorch = torch.softmax(x, dim=-1)
+print(f"Triton output:{out_triton}")
+print(f"PyTorch output:{out_pytorch}")
 
 assert torch.allclose(out_triton, out_pytorch, atol=1e-2)
 print("Correct!")
